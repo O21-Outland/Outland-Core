541d5347050506195d1ef29333f933247ffd97d4
Neronix17 <39186138+neronix17@users.noreply.github.com>
2025-06-13 19:40:45 +0100

    Initial 1.6
    Waiting on VEF to be able to test fully.

---
+++ 1.6/Mods/RoMBones/Source/Outland_RoMBones/Patch_HarmonyPatches_ButcherProducts_Postfix.cs
@@ -0,0 +1,24 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+using BoneMod;
+using HarmonyLib;
+
+namespace Outland_RoMBones
+{
+	[HarmonyPatch(typeof(HarmonyPatches), "ButcherProducts_PostFix")]
+	public class Patch_HarmonyPatches_ButcherProducts_PostFix
+	{
+		[HarmonyPrefix]
+		public static bool Prefix()
+		{
+			return false;
+		}
+	}
+}
